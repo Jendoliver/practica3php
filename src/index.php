b13dee164ca94272bd882ac730d4b4b11d50afa5
Jhen D'Òliver Morious <jandol1996@hotmail.com>
2017-01-18 19:22:12 +0000

    Creados ficheros

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>STUCOM BASKET</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Bienvenido a Stucom Basket</h1>
+        <a href="crearequipo.php">Nuevo Equipo</a><br>
+        <a href="crearjugador.php">Nuevo Jugador</a><br>
+        <a href="modificarjugador.php">Modificar canastas, asistencias y rebotes de un jugador</a><br>
+        <a href="modificarequipo.php">Modificar el equipo de un jugador determinado.</a><br>
+        <a href="borrarjugador.php">Borrar un jugador de la bbdd.</a><br>
+        <a href="listarjugadores.php">Listado de todos los jugadores.</a><br>
+        <a href="listarequipos.php">Listado de todos los equipos.</a><br>
+        <a href="listarjugadoresequipo.php">Listado de todos los jugadores de un equipo determinado.</a><br>
+        <a href="listarjugadorescanastas.php">Listado de jugadores que hayan conseguido un número mayor o igual a un número de canastas especificado por el usuario.</a><br>
+        <a href="listarequipociudad.php">Nombre de todos los equipos de una ciudad determinada.</a><br>
+    </body>
+</html>
